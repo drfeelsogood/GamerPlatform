e2500e29f8b54c1122ebf34b384eacfa6217f652
drfeelsogood <erenevirgen@outlook.com>
2016-02-05 20:26:24 +0200

    Forth Commit

---
+++ GamerPlatform/Business/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Business.Repository
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        IEnumerable<T> FindBy(Expression<Func<T, bool>> predicate);
+        T Add(T entity);
+        T Delete(T entity);
+        void Edit(T entity);
+        void Save();
+    }
+}
