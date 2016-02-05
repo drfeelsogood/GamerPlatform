31b8fe8ece9f9d1579456a527eb55e398e293894
drfeelsogood <erenevirgen@outlook.com>
2016-02-06 00:26:55 +0200

    Third Commit - Begin Admin Areas and Bilgisayar Category

--- GamerPlatform/GamerPlatformUI/Controllers/HomeController.cs
+++ GamerPlatform/GamerPlatformUI/Controllers/HomeController.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace GamerPlatformUI.Areas.Admin.Controllers
+namespace GamerPlatformUI.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : SiteBaseController
     {
         public ActionResult Index()
         {
---
+++ GamerPlatform/GamerPlatformUI/Controllers/SiteBaseController.cs
@@ -0,0 +1,36 @@
+﻿using Data.Context;
+using Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Business.Repository; 
+
+namespace GamerPlatformUI.Controllers
+{
+    public class SiteBaseController : Controller
+    {
+        protected YorGameContext db;
+        Repository<Category> rep = new Repository<Category>();
+        public SiteBaseController()
+        {
+            db = new YorGameContext();
+        }
+        public PartialViewResult GetBilgisayar()
+        {
+            List<Category> catList = rep.FindBy(x => x.ParentID == 6).ToList();
+            List<Category> catList2 = rep.FindBy(x => x.ParentID == 12).ToList();
+            Category cat = rep.FindByID(6);
+            ViewBag.cat = cat.Name;
+            ViewData["childList"] = catList2;
+            return PartialView("~/Views/SiteShared/SitePartials/BilgisayarPartial.cshtml", catList);
+        }
+        public PartialViewResult GetCategory()
+        {
+            List<Category> catList = rep.FindBy(x => x.ParentID == 6).ToList();
+            Category cat = rep.FindByID(6);
+            ViewBag.cat = cat.Name;
+            return PartialView("~/Views/SiteShared/SitePartials/CategoryPartial.cshtml", catList);
+        }
+
+    }
+}
